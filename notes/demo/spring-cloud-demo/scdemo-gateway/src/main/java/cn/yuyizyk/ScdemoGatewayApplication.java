90cfdf3ef733e9368f6c1ad98e93051757660b30
long <543625289@qq.com>
2018-11-29 17:59:41 +0800

    修改 spring boot 版本为 2.0.6 兼容 spring cloud f.m2版

---
+++ notes/demo/spring-cloud-demo/scdemo-gateway/src/main/java/cn/yuyizyk/ScdemoGatewayApplication.java
@@ -0,0 +1,24 @@
+package cn.yuyizyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringCloudApplication
+public class ScdemoGatewayApplication {
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes().route(r -> r.path("/baidu").uri("http://baidu.com:80/"))
+				.route("websocket_route", r -> r.path("/apitopic1/**").uri("ws://127.0.0.1:6605"))
+				.route(r -> r.path("/userapi3/**").filters(f -> f.addResponseHeader("X-AnotherHeader", "testapi3"))
+
+						.uri("lb://user-service/"))
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdemoGatewayApplication.class, args);
+	}
+}
--- notes/demo/spring-cloud-demo/scdemo-gateway/src/test/java/cn/yuyizyk/ScdemoGatewayApplicationTests.java
+++ notes/demo/spring-cloud-demo/scdemo-gateway/src/test/java/cn/yuyizyk/ScdemoGatewayApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ScdemoConfigServerApplicationTests {
+public class ScdemoGatewayApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Test
+	public void contextLoads() {
+	}
 
 }
